91c9ce361eadc14be87da2533cee52179ea1128a
ankhuve <erik.c.forsberg@gmail.com>
2015-03-09 16:44:00 +0100

    Create own goals page styling started

---
+++ create.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+		<meta name="mobile-web-app-capable" content="yes">
+		<title>GRÖN</title>
+		<link rel="icon" sizes="192x192" href="img/iconhd.png">
+		<link rel="icon" type="image/png" href="img/fav.png">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,100' rel='stylesheet' type='text/css'>    
+		<link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+		<script src="https://code.jquery.com/jquery.js"></script>
+		<script src="getsize.js"></script>
+	</head>
+<body>
+	<div class="container fullWidth goalBg">
+		<div class="wrapper">
+			<div id="goalHeaders">
+				<h1 class="mdMargin center" id="chooseGoalsHeader">Skapa egna mål</h1>
+				<h2 class="thinHeader center">10 poäng per mål</h2>
+			</div>
+		</div>
+		<div class="goalGrid center">
+			<div class="col-xs-12 newGoalForm">
+				<input class="userinput newGoal" type="goalDescription" placeholder="T.ex. duscha fem minuter kortare.">
+				<div class="createGoal"><a href="#">Skapa</a></div>
+			</div>
+		</div>
+		<div class="bottomGreen">
+			
+			<h2 class="goalInstructions"><a href="home.html">Gå vidare</a></h2>
+			<div class="navArrows">
+				<div class="prev">
+					<a href="goals.html">
+						<img class="arrow" src="img/prev_arrow_white.png">
+					</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
