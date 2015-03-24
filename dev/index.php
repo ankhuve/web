e807d5797f3f8315af5870cdea9fb2a6c027c1bf
ankhuve <erik.c.forsberg@gmail.com>
2015-03-24 14:15:44 +0100

    fix

--- dev/index.php
+++ dev/index.php
@@ -1,8 +1,6 @@
 <?php 
 	if(!isset($_COOKIE['userID'])){
 		header("Location: login.php");
-	} elseif($_COOKIE['userID']<50){
-		header('Location: php/logout.php');
 	}else {
 		include_once("php/config.php");
 		include_once("php/functions.php");
