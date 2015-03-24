2e41646f31b60cd09b2ba7652c91c61795c9d652
ankhuve <erik.c.forsberg@gmail.com>
2015-03-24 14:13:57 +0100

    elif fix

--- dev/index.php
+++ dev/index.php
@@ -1,7 +1,7 @@
 <?php 
 	if(!isset($_COOKIE['userID'])){
 		header("Location: login.php");
-	} else if($_COOKIE['userID']<50){
+	} elseif($_COOKIE['userID']<50){
 		header('Location: php/logout.php');
 	} else {
 		include_once("php/config.php");
